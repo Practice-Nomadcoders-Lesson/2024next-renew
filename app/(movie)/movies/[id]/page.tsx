f6ddbb10e9dc58a9b51e39eaf41509dda1b57ca9
iinsue <hinsu93@naver.com>
2024-02-12 17:54:13 +0900

    Server Component Parallel Requests

--- app/(movie)/movies/[id]/page.tsx
+++ app/(movie)/movies/[id]/page.tsx
@@ -1,6 +1,16 @@
 const getMovie = async (id: string) => {
+  console.log(`Fetching movies: ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 3000));
   const response = await fetch(`${process.env.NEXT_PUBLIC_GET_MOVIES}/${id}`);
+  return response.json();
+};
+
+const getVideos = async (id: string) => {
+  console.log(`Fetching videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_GET_MOVIES}/${id}/videos`
+  );
   return response.json();
 };
 
@@ -9,7 +19,9 @@ export default async function MovieDetail({
 }: {
   params: { id: string };
 }) {
-  const movie = await getMovie(id);
+  console.log("start fetching");
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  console.log("end fetching");
   return (
     <>
       <h1>{movie.title}</h1>
