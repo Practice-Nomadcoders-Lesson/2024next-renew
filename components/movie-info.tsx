894d455dba7a4256f466f97409df69595ce791d6
iinsue <50567588+iinsue@users.noreply.github.com>
2024-02-18 22:32:51 +0900

    Add Dynamic Metadata

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,6 +1,6 @@
 import styles from "../styles/movie-info.module.css";
 
-const getMovie = async (id: string) => {
+export const getMovie = async (id: string) => {
   await new Promise((resolve) => setTimeout(resolve, 3000));
   const response = await fetch(`${process.env.NEXT_PUBLIC_GET_MOVIES}/${id}`);
   return response.json();
